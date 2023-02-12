3694fca5960d816cf936232ead4b73d31aeb0742
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-02-12 12:09:10 -0800

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,60 +1,62 @@
 #!/usr/bin/python3
-'''
-Contains all the Base model for all the other classes
-'''
-import models
-import uuid
+"""Defines a base model class.
+This class defines all common attributes/methods for other classes.
+"""
+# Imports
 from datetime import datetime
+from uuid import uuid4
+import models
 
 
 class BaseModel:
-    '''
-    The base class for all the other classes
-    '''
+    """Represents the "base" for all other classes."""
 
+    # kwargs is a dictionary
     def __init__(self, *args, **kwargs):
-        '''
-        Initializes the base model
-        '''
+        """Initialize a new BaseModel/ Instance
+        Args:
+            id (int): Unique id for each BaseModel
+            created_at: Date of object creation
+            updated_at: Date of object change
+        """
+        # tform = "%Y-%m-%dT%H:%M:%S.%f"
+
         if kwargs:
-            kwargs.pop("__class__")
-            for arg, value in kwargs.items():
-                if arg == "created_at" or arg == "updated_at":
-                    date_obj = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, arg, date_obj)
-                else:
-                    setattr(self, arg, value)
+            # Create from dictionary, Loop dictionary key and values
+            for key, value in kwargs.items():
+                # Set object attributes dynamically using setattr function
+                if(key == "__class__"):
+                    continue
+
+                # Convert isoformat string date to datetime object
+                if key in ("created_at", "updated_at"):
+                    value = datetime.fromisoformat(value)
+
+                setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
             models.storage.new(self)
 
-    def __str__(self):
-        '''
-        Returns string representation of the object
-        '''
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
-
     def save(self):
-        '''
-        Updates the public instance attribute updated at
-        with the current datetime
-        '''
+        """Updates the public instance attribute updated_at with
+            the current datetime.
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        '''
-        Returns a dictionary containing a representation of the instance
-        '''
-        obj_dict = {}
-        obj_dict['__class__'] = self.__class__.__name__
-        for k, v in self.__dict__.items():
-            if k == "created_at" or k == "updated_at":
-                date_str = v.isoformat()
-                obj_dict[k] = date_str
-            else:
-                obj_dict[k] = v
-        return obj_dict
+        """Return a dictionary containing all keys/values of __dict__ of
+            the instance
+        """
+        dictionary = self.__dict__.copy()
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary["__class__"] = self.__class__.__name__
+        return dictionary
+
+    def __str__(self):
+        """Return the printable representation of model"""
+        return "[{}] ({}) {}" \
+            .format(self.__class__.__name__, self.id, self.__dict__)
